aecc606cca252b3d6f93d63c9c092ae6ff197497
ITCSIU21009 <118718862+ITCSIU21009@users.noreply.github.com>
2023-05-02 14:59:39 +0700

    Add Player and MonopolySquare

---
+++ Monopoly/RunGame/Player.java
@@ -0,0 +1,36 @@
+public class Player {
+  private String name;
+  private int balance;
+  private int position;
+
+
+  public Player(String name, int balance, int position) {
+    this.name = name;
+    this.balance = balance;
+    this.position = position;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getBalance() {
+    return balance;
+  }
+
+  public int getPosition() {
+    return position;
+  }
+
+  public void setPosition(int position) {
+    this.position = position;
+  }
+
+  public void addBalance(int amount) {
+    balance += amount;
+  }
+
+  public void subtractBalance(int amount) {
+    balance -= amount;
+  }
+}
