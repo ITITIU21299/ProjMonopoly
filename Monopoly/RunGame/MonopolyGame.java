a6e156ffda640556386712b3d9932bf8facd2510
ITITIU21299 <matofottv@gmail.com>
2023-05-16 18:46:45 +0700

    Notification

--- Monopoly/RunGame/MonopolyGame.java
+++ Monopoly/RunGame/MonopolyGame.java
@@ -1,9 +1,11 @@
 package Monopoly.RunGame;
 
+import Monopoly.RunGame.GameRule.Player;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.FlowLayout;
+import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import javax.swing.JButton;
@@ -17,11 +19,32 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
 
-public class MonopolyGame {
+public class MonopolyGame{
         private boolean gameEnd=false;
         private JFrame frame;
         private int bankrupted;
         private final int fwidth=800, fheight=800;
+        private boolean isPlayerTurn;
+        DiceLabel label = new DiceLabel();
+
+        Random random = new Random();
+
+        Dice diceButton = new Dice();
+        ImageIcon[] dice = new ImageIcon[10];
+        Image[] icon = new Image[10];
+        Image[] resizedicon = new Image[10];
+        Icon[] dIcon = new Icon[10];
+        
+        int CurrentPlayerIndex = 0;
+        
+        Notification notification = new Notification();
+        
+        int count=0;
+
+        Player[] players = {new Player("Player 1",1000,0,"Monopoly/res/token_car.png"),
+        new Player("Player 2",1000,0,"Monopoly/res/token_dog.png"),
+        new Player("Player 3",1000,0,"Monopoly/res/token_hat.png"),
+        new Player("Player 4",1000,0,"Monopoly/res/token_ship.png")};
         
     public void setup(){
         frame = new JFrame("Monopoly");
@@ -34,7 +57,9 @@ public class MonopolyGame {
             public void actionPerformed(ActionEvent e) {
                 startGame();
             }
+        
         });
+
 
         StartButton.setIcon(icon);
         frame.setLayout(null);
@@ -59,25 +84,68 @@ public class MonopolyGame {
         game.setup();
     }
     private void startGame(){
+        
+        //Image image = new ImageIcon("Monopoly/res/rollingDice.gif").getImage().getScaledInstance(178, 125, java.awt.Image.SCALE_SMOOTH);
+        //ImageIcon rollingDice = new ImageIcon(image);
+        //ImageIcon rollingDice = new ImageIcon("Monopoly/res/rollingDice.gif");
+
         frame.getContentPane().removeAll();
         
         JPanel gameDisplay = new GameDisplay(fwidth, fheight);
         gameDisplay.setBounds(0, 0, fwidth, fheight);
         
-        Notification notification = new Notification();
+        
         notification.setBounds(800, 0, 600, 420);
 
-        DiceLabel label = new DiceLabel();
-        label.setLayout(new BorderLayout(0, 0));
+        dice[1] = new ImageIcon("Monopoly/res/dice1.png");    
+        dice[2] = new ImageIcon("Monopoly/res/dice2.png");
+        dice[3] = new ImageIcon("Monopoly/res/dice3.png");
+        dice[4] = new ImageIcon("Monopoly/res/dice4.png");
+        dice[5] = new ImageIcon("Monopoly/res/dice5.png");
+        dice[6] = new ImageIcon("Monopoly/res/dice6.png");
 
-        Dice diceButton = new Dice();
-        RollDice rollDice = new RollDice(diceButton, label);
+        for (int i=1;i<=6;i++) {
+            icon[i] = dice[i].getImage();
+            resizedicon[i] = icon[i].getScaledInstance(120, 120, java.awt.Image.SCALE_SMOOTH);
+            dIcon[i] = new ImageIcon(resizedicon[i]);
+        }
+        
+
+        gameEnd=false;
+
+        
+
+        
+
+        JPanel drawP1 = new DrawPlayer(players[0],0);
+
+        //while (!gameEnd){
+            
+
+
+
+            isPlayerTurn = true;
+            diceButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if (!gameEnd){
+                        Player currentplayer = players[CurrentPlayerIndex];
+                        PlayTurn(currentplayer);
+                    }
+               }
+            });
+
+            if (bankrupted==3){
+            gameEnd=true;}
+        //}
+        
 
         JPanel dicePanel = new JPanel();
         dicePanel.setBounds(fwidth+80, fheight/2, 400, fheight/2);
-        dicePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 20));
-        dicePanel.add(diceButton);
-        dicePanel.add(label);
+        dicePanel.setLayout(new BorderLayout(0, -150));
+        dicePanel.add(diceButton, BorderLayout.SOUTH);
+        dicePanel.add(label, BorderLayout.CENTER);
+
+
 
         frame.revalidate();
         frame.repaint();
@@ -89,4 +157,28 @@ public class MonopolyGame {
 
         //startNewGame();
     }
+    public void PlayTurn(Player CPlayer){
+        int result1 = random.nextInt(6) +1;
+        int result2 = random.nextInt(6) +1;
+        Dice.setResult(result1, result2);
+        label.setIcon(new TwoIcon(dIcon[result1], dIcon[result2]));
+        int result=result1+result2;
+        if (count==10){
+            notification.RemoveNotification();
+            count=0;
+        }
+
+        notification.addNotification("                                " + CPlayer.getName()+" move "+ result + " steps                                  ");
+        count++;
+        //notification.RemoveNotification();
+
+        
+        if (bankrupted==3){
+            gameEnd=true;
+        }
+        CurrentPlayerIndex++;
+        if (CurrentPlayerIndex==4)
+            CurrentPlayerIndex=0;
+    }
+    
 }
--- Monopoly/RunGame/Notification.java
+++ Monopoly/RunGame/Notification.java
@@ -7,14 +7,28 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 public class Notification extends JPanel{
+    JLabel notificationLabel;
     public void addNotification(String text) {
-        JLabel notificationLabel = new JLabel(text);
+        
+        notificationLabel = new JLabel(text);
         notificationLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         notificationLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        
         add(notificationLabel);
 
         revalidate();
         repaint();
     }
-    
+    public void RemoveNotification(){
+        //remove(notificationLabel);
+        Component[] components = getComponents();
+        for (Component component : components) {
+            if (component instanceof JLabel) {
+                remove(component);
+            }
+    }
+        
+        revalidate();
+        repaint();
+    }
 }
